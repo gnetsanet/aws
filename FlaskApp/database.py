5f37d14a220b35d4483c6292a5e50597b10d3e31
Netsanet Gebremedhin <gnetsanet@gmail.com>
2018-03-09 18:13:58 +0000

    Cognito related changes.

--- FlaskApp/database.py
+++ FlaskApp/database.py
@@ -12,22 +12,34 @@
 import mysql.connector
 import config
 
-def list_photos():
+def list_photos(cognito_username):
     "Select all the photos from the database"
     conn = get_database_connection()
     cursor = conn.cursor(dictionary=True)
-    cursor.execute("""SELECT object_key, labels, created_datetime FROM photo
-                      WHERE cognito_username is null""")
+    cursor.execute("""SELECT object_key, description, labels, created_datetime
+        FROM photo WHERE cognito_username = %s
+        ORDER BY created_datetime desc""", (cognito_username,))
     result = cursor.fetchall()
     cursor.close()
     conn.close()
     return result
 
-def add_photo(object_key, labels):
+def add_photo(object_key, labels, description, cognito_username):
     "Add a photo to the database"
     conn = get_database_connection()
     cursor = conn.cursor(dictionary=True)
-    cursor.execute("INSERT INTO photo (object_key, labels) VALUES (%s, %s);", (object_key, labels))
+    cursor.execute("""INSERT INTO photo (object_key, labels, description, cognito_username) VALUES
+    (%s, %s, %s, %s);""", (object_key, labels, description, cognito_username))
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+def delete_photo(object_key, cognito_username):
+    "Delete a photo.  Users can only delete their photos!"
+    conn = get_database_connection()
+    cursor = conn.cursor()
+    cursor.execute("""DELETE FROM photo WHERE object_key = %s AND cognito_username = %s;""",
+                   (object_key, cognito_username))
     conn.commit()
     cursor.close()
     conn.close()
